b017da0e33d596538252ed586d5ac7769db53e6f
verarr <verarr@fvnki.town>
2024-10-10 23:14:07 +0200

    add a description for time modifiers

--- src/main/java/xyz/verarr/synchrono/config/SynchronoConfig.java
+++ src/main/java/xyz/verarr/synchrono/config/SynchronoConfig.java
@@ -37,6 +37,7 @@ public class SynchronoConfig extends MidnightConfig {
     @Comment(category = GAME_TIME) public static Comment gametime_spacer1;
 
     @Comment(category = GAME_TIME, centered = true) public static Comment time_modifiers_title;
+    @Comment(category = GAME_TIME) public static Comment time_modifiers_description;
     @Entry(category = GAME_TIME) public static float scalar = 1.0f;
     @Entry(category = GAME_TIME) public static int offset_ticks = 0;
 
