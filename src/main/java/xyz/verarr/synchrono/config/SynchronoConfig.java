12be421d69598a2b42d4c4a80b1325fab7370cc0
verarr <verarr@fvnki.town>
2024-10-14 22:31:59 +0200

    remove comments from old config

--- src/main/java/xyz/verarr/synchrono/config/SynchronoConfig.java
+++ src/main/java/xyz/verarr/synchrono/config/SynchronoConfig.java
@@ -17,47 +17,22 @@ public class SynchronoConfig extends MidnightConfig {
     public static final String GAME_TIME = "c_game_time";
     public static final String DEBUG = "d_debug";
 
-    @Comment(category = TIME, centered = true) public static Comment coords_title;
-    @Comment(category = TIME) public static Comment coords_description;
-
     @Entry(category = TIME, min = -90d, max = 90d) public static double latitude = 51.11d;
     @Entry(category = TIME, min = -180d, max = 180d) public static double longitude = 17.022222d;
     @Entry(category = TIME) public static String timezone = "UTC";
 
-    @Comment(category = TIME) public static Comment time_spacer1;
-
-    @Comment(category = TIME, centered = true) public static Comment time_misc_title;
-
-    @Comment(category = TIME) public static Comment invert_description;
     @Entry(category = TIME) public static boolean invert = false;
 
-    @Comment(category = GAME_TIME) public static Comment gametime_enabled_description;
     @Entry(category = GAME_TIME) public static boolean gametime_enabled = true;
 
-    @Comment(category = GAME_TIME) public static Comment gametime_spacer1;
-
-    @Comment(category = GAME_TIME, centered = true) public static Comment time_modifiers_title;
-    @Comment(category = GAME_TIME) public static Comment time_modifiers_description;
-    @Comment(category = GAME_TIME) public static Comment time_modifiers_description_spacer;
     @Entry(category = GAME_TIME) public static float scalar = 1.0f;
     @Entry(category = GAME_TIME) public static int offset_ticks = 0;
 
-    @Comment(category = DEBUG) public static Comment debug_description;
-
-    @Comment(category = DEBUG) public static Comment debug_spacer1;
-
     @Entry(category = DEBUG) public static boolean brute_force = false;
 
-    @Comment(category = DEBUG) public static Comment debug_spacer2;
-
-    @Comment(category = DEBUG, centered = true) public static Comment api_properties_title;
-    @Comment(category = DEBUG) public static Comment api_properties_description;
     @Entry(category = DEBUG) public static String sunrise_property = "sunrise";
     @Entry(category = DEBUG) public static String sunset_property = "sunset";
 
-    @Comment(category = DEBUG) public static Comment debug_spacer3;
-
-    @Comment(category = DEBUG, centered = true) public static Comment debug_toggles_title;
     @Entry(category = DEBUG) public static boolean set_time = true;
     @Entry(category = DEBUG) public static boolean set_rate = true;
 }
