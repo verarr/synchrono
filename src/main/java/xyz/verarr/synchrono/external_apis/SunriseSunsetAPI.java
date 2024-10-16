8a8990a8c696946620bbd5f82203f9fb0051c6b8
verarr <verarr@fvnki.town>
2024-10-16 21:59:32 +0200

    migrate old http code to new helper

--- src/main/java/xyz/verarr/synchrono/external_apis/SunriseSunsetAPI.java
+++ src/main/java/xyz/verarr/synchrono/external_apis/SunriseSunsetAPI.java
@@ -6,11 +6,6 @@ import com.google.gson.JsonSyntaxException;
 import org.jetbrains.annotations.NotNull;
 import xyz.verarr.synchrono.config.SynchronoConfig;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
@@ -33,30 +28,11 @@ public class SunriseSunsetAPI {
             throw new RuntimeException(e);
         }
 
-        HttpURLConnection conn;
-        try {
-            conn = (HttpURLConnection) uri.toURL().openConnection();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            conn.setRequestMethod("GET");
-        } catch (ProtocolException e) {
-            throw new RuntimeException(e);
-        }
+        String result = HTTPHelper.get(uri);
 
-        StringBuilder result = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(conn.getInputStream()))) {
-            for (String line; (line = reader.readLine()) != null; ) {
-                result.append(line);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
         JsonObject jsonObject;
         try {
-            jsonObject = JsonParser.parseString(result.toString()).getAsJsonObject();
+            jsonObject = JsonParser.parseString(result).getAsJsonObject();
         } catch (JsonSyntaxException e) {
             throw new RuntimeException(e + " URL: " + uri + " JSON: " + result);
         }
