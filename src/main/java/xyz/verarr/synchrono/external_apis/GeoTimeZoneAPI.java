8a335257d09467f53bb58a81460814b6b173d250
verarr <verarr@fvnki.town>
2024-10-16 20:05:38 +0200

    add GeoTimeZone api clas

---
+++ src/main/java/xyz/verarr/synchrono/external_apis/GeoTimeZoneAPI.java
@@ -0,0 +1,42 @@
+package xyz.verarr.synchrono.external_apis;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.ProtocolException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.ZoneId;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class GeoTimeZoneAPI {
+    private static final String API_URL = "https://api.geotimezone.com/public/timezone";
+
+    public static @NotNull ZoneId query(double latitude, double longitude) {
+        URI uri;
+        try {
+            Formatter formatter = new Formatter(Locale.ROOT);
+            uri = new URI(API_URL + formatter.format("?latitude=%f&longitude=%f", latitude, longitude));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        String result = HTTPHelper.get(uri);
+
+        JsonObject jsonObject;
+        try {
+            jsonObject = JsonParser.parseString(result).getAsJsonObject();
+        } catch (JsonSyntaxException e) {
+            throw new RuntimeException(e + " URL: " + uri.toString() + " JSON: " + result.toString());
+        }
+
+        return ZoneId.of(jsonObject.get("timezone_abbreviation").getAsString());
+    }
+}
