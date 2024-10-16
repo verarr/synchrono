74076e0ba6c4619fccdf9d993753793f11a41a97
verarr <verarr@fvnki.town>
2024-10-16 16:55:06 +0200

    cosmetic sleep messages

--- src/main/java/xyz/verarr/synchrono/mixin/PreventSleepMixin.java
+++ src/main/java/xyz/verarr/synchrono/mixin/PreventSleepMixin.java
@@ -3,6 +3,7 @@ package xyz.verarr.synchrono.mixin;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.SleepManager;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -12,5 +13,14 @@ public class PreventSleepMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/SleepManager;canSkipNight(I)Z"))
     public boolean preventSleeping(SleepManager instance, int percentage) {
         return false; // always prevent sleeping
+    }
+
+    /**
+     * @author verarr
+     * @reason cosmetic messages relating to sleep
+     */
+    @Overwrite()
+    public boolean isSleepingEnabled() {
+        return false; // always say can't sleep
     }
 }
