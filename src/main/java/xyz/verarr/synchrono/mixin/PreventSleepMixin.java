b411ac53a7bb74246c8cc4925e7905dcf2535c91
verarr <verarr@fvnki.town>
2024-10-16 17:03:08 +0200

    add config toggle

--- src/main/java/xyz/verarr/synchrono/mixin/PreventSleepMixin.java
+++ src/main/java/xyz/verarr/synchrono/mixin/PreventSleepMixin.java
@@ -1,26 +1,29 @@
 package xyz.verarr.synchrono.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.SleepManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import xyz.verarr.synchrono.config.SynchronoConfig;
 
 @Mixin(ServerWorld.class)
 public class PreventSleepMixin {
-    @Redirect(method = "tick(Ljava/util/function/BooleanSupplier;)V",
+    @ModifyExpressionValue(method = "tick(Ljava/util/function/BooleanSupplier;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/SleepManager;canSkipNight(I)Z"))
-    public boolean preventSleeping(SleepManager instance, int percentage) {
-        return false; // always prevent sleeping
+    public boolean preventSleeping(boolean original) {
+        return SynchronoConfig.prevent_sleep ? false : original;
     }
 
     /**
      * @author verarr
      * @reason cosmetic messages relating to sleep
      */
-    @Overwrite()
-    public boolean isSleepingEnabled() {
-        return false; // always say can't sleep
+    @ModifyReturnValue(method = "isSleepingEnabled()Z", at = @At("RETURN"))
+    public boolean isSleepingEnabled(boolean original) {
+        return SynchronoConfig.prevent_sleep ? false : original;
     }
 }
