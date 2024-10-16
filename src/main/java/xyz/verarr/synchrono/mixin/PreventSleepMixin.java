d942897d5cafac8445deb8e72423c058cd6824db
verarr <verarr@fvnki.town>
2024-10-16 16:36:24 +0200

    add mixin that prevents passing the night

---
+++ src/main/java/xyz/verarr/synchrono/mixin/PreventSleepMixin.java
@@ -0,0 +1,16 @@
+package xyz.verarr.synchrono.mixin;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.world.SleepManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerWorld.class)
+public class PreventSleepMixin {
+    @Redirect(method = "tick(Ljava/util/function/BooleanSupplier;)V",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/SleepManager;canSkipNight(I)Z"))
+    public boolean preventSleeping(SleepManager instance, int percentage) {
+        return false; // always prevent sleeping
+    }
+}
