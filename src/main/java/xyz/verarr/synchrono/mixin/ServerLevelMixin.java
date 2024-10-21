5f70df7a8385d1a55560fece8097602d6c918294
verarr <verarr@fvnki.town>
2024-10-22 00:15:02 +0200

    Update time less (#55)
    * update only if 30 minutes have passed on the system clock
    
    * also update time if suspected to be out of sync
    
    suspected is if when the server ticks since last update is 2x or more the wall clock time since last update in ticks
    
    * drunk stuff :beer: (ps. single worst commit in this repo so far)
    
    * only update on time tick
    
    makes only active dimensions update time (maybe..?)

--- src/main/java/xyz/verarr/synchrono/mixin/ServerLevelMixin.java
+++ src/main/java/xyz/verarr/synchrono/mixin/ServerLevelMixin.java
@@ -22,7 +22,9 @@ import xyz.verarr.synchrono.IRLTimeManager;
 import xyz.verarr.synchrono.Synchrono;
 import xyz.verarr.synchrono.config.SynchronoConfig;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.function.BooleanSupplier;
@@ -31,6 +33,9 @@ import java.util.function.BooleanSupplier;
 public class ServerLevelMixin {
     @Shadow @Final private ServerWorldProperties worldProperties;
     @Unique private IRLTimeManager irlTimeManager;
+
+    @Unique private Instant lastUpdateTime;
+    @Unique private long lastUpdateTimeTicks;
 
     @Inject(method = "<init>", at = @At("TAIL"))
     public void initializeIRLTimeManager(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey<World> worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld, long seed, List<SpecialSpawner> spawners, boolean shouldTickTime, RandomSequencesState randomSequencesState, CallbackInfo ci) {
@@ -41,6 +46,9 @@ public class ServerLevelMixin {
     @Unique
     public void updateTime() {
         if (!SynchronoConfig.gametimeEnabled) return;
+
+        lastUpdateTime = Instant.now();
+        lastUpdateTimeTicks = worldProperties.getTime();
 
         TimeManager timeManager = TimeManager.getInstance((ServerWorld) (Object) this);
         LocalDateTime now = LocalDateTime.now(SynchronoConfig.timezone());
@@ -63,13 +71,23 @@ public class ServerLevelMixin {
         updateTime();
     }
 
-    @Inject(method = "tick", at = @At("TAIL"))
-    public void periodicallyUpdateTime(BooleanSupplier shouldKeepTicking, CallbackInfo ci) {
-        if (
-                (worldProperties.getTime() % 12000 == 0) ||
-                        SynchronoConfig.bruteForce
-        ) {
-            updateTime();
-        }
+    @Inject(method = "tickTime", at = @At("TAIL"))
+    public void periodicallyUpdateTime(CallbackInfo ci) {
+        String reason;
+
+        long minutesSinceLastUpdate = ChronoUnit.MINUTES.between(lastUpdateTime, Instant.now());
+        long serverTicksSinceLastUpdate = worldProperties.getTime() - lastUpdateTimeTicks;
+        long wallClockTicksSinceLastUpdate = (ChronoUnit.MILLIS.between(lastUpdateTime, Instant.now()) * 20) / 1000;
+
+        if (minutesSinceLastUpdate >= 30)
+            reason = "30 wall clock minutes have passed since last update";
+        else if (wallClockTicksSinceLastUpdate >= serverTicksSinceLastUpdate + 200)
+            reason = "Time out of sync (" + wallClockTicksSinceLastUpdate + " >= " + serverTicksSinceLastUpdate + " + 200)";
+        else if (SynchronoConfig.bruteForce)
+            reason = "Brute-force mode enabled";
+        else return;
+
+        updateTime();
+        Synchrono.LOGGER.info("Time update triggered because: {}", reason);
     }
 }
