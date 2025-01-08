a5f36792af4f80d491fd0047dab06448ba6b39ff
verarr <verarr@fvnki.town>
2025-01-08 18:57:26 +0100

    Adjusted Phantom Spawns integration (#60)
    * add dependencies
    * add config value
    * add mixin
    * add gui config

---
+++ src/main/java/xyz/verarr/synchrono/mixin/integration/AdjustedPhantomSpawnsMixin.java
@@ -0,0 +1,18 @@
+package xyz.verarr.synchrono.mixin.integration;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+
+import xyz.verarr.adjusted_phantom_spawns.GameRuleHelper;
+import xyz.verarr.synchrono.config.SynchronoConfig;
+
+@Mixin(GameRuleHelper.class)
+public class AdjustedPhantomSpawnsMixin {
+    @WrapMethod(method = "getPhantomSpawningThreshold", remap = false)
+    private int wrapGetPhantomSpawningThreshold(Operation<Integer> original) {
+        if (SynchronoConfig.adjustedPhantomSpawnsIntegration) return 1728000;
+        else return original.call();
+    }
+}
